4b4f80adba3bde6498334e2dfdbe7b38fa5131a1
kratos23 <pavel.krylov.03@gmail.com>
2024-05-26 21:35:59 +0300

    Enabled all paramethers should be documented check

---
+++ src/NUnitFramework/nunitlite/GlobalSuppressions.cs
@@ -0,0 +1,17 @@
+// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1611:Element parameters should be documented", Justification = "<Pending>", Scope = "member", Target = "~M:NUnit.Common.CommandLineOptions.RequiredValue(System.String,System.String,System.String[])~System.String")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1611:Element parameters should be documented", Justification = "<Pending>", Scope = "member", Target = "~M:NUnit.Common.ExtendedTextWrapper.Dispose(System.Boolean)")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1611:Element parameters should be documented", Justification = "<Pending>", Scope = "member", Target = "~M:NUnit.Common.ExtendedTextWrapper.Write(System.Char)")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1611:Element parameters should be documented", Justification = "<Pending>", Scope = "member", Target = "~M:NUnit.Common.ExtendedTextWrapper.Write(System.String)")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1611:Element parameters should be documented", Justification = "<Pending>", Scope = "member", Target = "~M:NUnit.Common.ExtendedTextWrapper.WriteLine(System.String)")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1611:Element parameters should be documented", Justification = "<Pending>", Scope = "member", Target = "~M:NUnit.Common.TestSelectionParserException.#ctor(System.Runtime.Serialization.SerializationInfo,System.Runtime.Serialization.StreamingContext)")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1611:Element parameters should be documented", Justification = "<Pending>", Scope = "member", Target = "~M:NUnit.Common.TestSelectionParserException.#ctor(System.String)")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1611:Element parameters should be documented", Justification = "<Pending>", Scope = "member", Target = "~M:NUnitLite.NUnitLiteOptions.#ctor(System.String[])")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1611:Element parameters should be documented", Justification = "<Pending>", Scope = "member", Target = "~M:NUnitLite.OutputWriter.WriteResultFile(NUnit.Framework.Interfaces.ITestResult,System.String,System.Collections.Generic.IDictionary{System.String,System.Object},NUnit.Framework.Internal.TestFilter)")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1611:Element parameters should be documented", Justification = "<Pending>", Scope = "member", Target = "~M:NUnitLite.TextRunner.MakeRunSettings(NUnitLite.NUnitLiteOptions)~System.Collections.Generic.Dictionary{System.String,System.Object}")]
---
+++ src/NUnitFramework/tests/GlobalSuppressions.cs
@@ -0,0 +1,11 @@
+// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1611:Element parameters should be documented", Justification = "<Pending>", Scope = "member", Target = "~M:NUnit.Framework.Tests.Internal.Filters.AndFilterTests.CombineTest(System.Collections.Generic.IEnumerable{System.Boolean},System.Boolean,NUnit.Framework.Tests.Internal.Filters.MockTestFilter.MatchFunction)")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1611:Element parameters should be documented", Justification = "<Pending>", Scope = "member", Target = "~M:NUnit.Framework.Tests.Attributes.RangeAttributeTests.RangeWithExpectedConversions.AssertCoercionErrorOrMatchingSequence(System.Type,System.Collections.IEnumerable)")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1611:Element parameters should be documented", Justification = "<Pending>", Scope = "member", Target = "~M:NUnit.Framework.Tests.Internal.Filters.OrFilterTests.CombineTest(System.Collections.Generic.IEnumerable{System.Boolean},System.Boolean,NUnit.Framework.Tests.Internal.Filters.MockTestFilter.MatchFunction)")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1611:Element parameters should be documented", Justification = "<Pending>", Scope = "member", Target = "~M:NUnit.Framework.Tests.TestUtilities.UniqueValues.Check``1(NUnit.Framework.Constraints.ActualValueDelegate{``0},System.Int32,System.Int32)")]
